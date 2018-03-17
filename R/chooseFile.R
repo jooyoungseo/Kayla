974c152fe5b84d2c92cf67df661a34829013e196
JooYoung Seo <sjysky@gmail.com>
2018-03-17 16:59:11 -0400

    V0.1.1: chooseFile function starts supporting *.csv, *.txt, *.xlsx, and *.sav file formats automatiaclly.

--- R/chooseFile.R
+++ R/chooseFile.R
@@ -4,10 +4,10 @@
 #' @aliases chooseFile
 #' @keywords chooseFile
 
-#' @description You can alternatively use this function for choosing CSV file in an interactive GUI mode A file choose dialog box will be prompted.
+#' @description You can alternatively use this function for choosing *.csv, *.txt, *.xlsx, *.sav (SPSS) files in an interactive GUI mode A file choose dialog box will be prompted.
 
 #' @export chooseFile
-#' @param ... Any arguments available for read.csv() function; file will be automatically chosen once you pick up your file using the file chooser dialog box.
+#' @param ... Any arguments available for read.csv(), read.table(), readxl::read_excel(), foreign::read.spss() functions; file will be automatically chosen once you pick up your file using the file chooser dialog box.
 
 #' @details
 #' See example below.
@@ -16,17 +16,30 @@
 
 #' @examples
 
-#' ```{r, echo=TRUE}
 #' data <- chooseFile(header=T)
 #' str(data)
 #' # You can  drop off your file using the file picker dialog box.
-#' ```
 
-#' @author JooYoung Seo
+#' @author JooYoung Seo (jooyoung@psu.edu)
 
 #Function starts
 chooseFile <-
 function(...) {
-read.csv(file.choose(), ...)
+
+fullFile <- file.choose()
+fileExt <- tools::file_ext(fullFile)
+
+if(fileExt == "csv") {
+read.csv(fullFile, ...)
+} else if(fileExt == "txt") {
+read.table(fullFile, ...)
+} else if(fileExt == "xlsx") {
+readxl::read_excel(fullFile, ...)
+} else if(fileExt == "sav") {
+foreign::read.spss(file=fullFile, to.data.frame=T, ...)
+} else {
+stop("Sorry, but the file you have just chosen is not supported in this function. Report on this issue to the author (JooYoung Seo) at jooyoung@psu.edu. :)")
+}
+
 }
 #Function ends
--- inst/NEWS.md
+++ inst/NEWS.md
@@ -1,4 +1,12 @@
 
+# Kayla 0.1.1
+
+## Improvements for chooseFile()
+
+- It supports *.csv, *.txt, *.xlsx,, and *.sav files.
+- chooseFile() can automatically wrap the given file path for *csv, *.txt, *.xlsx, and *.sav.
+
+
 # Kayla 0.1.0
 
 - Initial release on GitHub
